5b7ab08ced002bb008dc3f30b46810220d480821
Lonny Wong <lonnywong@qq.com>
2022-01-18 23:27:04 +0800

    Initial Commit

---
+++ TmuxSendKeys/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TmuxSendKeys
+//
+//  Created by Lonny Wong on 2022/1/18.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
